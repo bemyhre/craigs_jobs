a006c356b2f048bdd1aa2e290d63284a76d10a30
Ryland Herrick <ryland@ry-land.com>
2011-05-15 22:53:00 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Craigslist Job Scraper
+
+My first Ruby script.  Written when I was perusing craigslist daily for jobs, and wanted several categories and cities on one page. 
+
+Currently it takes posts from Madison and Milwaukee, WI, from the categories of software, systems, tech support, and web design. 
+
+##Usage
+simply run the script with `ruby cl.rb` and it'll generate an html file in the same directory called cl.html.
+
+
+##Dependencies
+rubygems, mechanize, and time
---
+++ cl.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'mechanize'
+require 'time'
+
+class Job
+  attr_accessor :loc, :area, :cat, :date, :text, :link
+  def initialize(loc, area, cat, date, text, link)
+    @loc = loc
+    @area = area
+    @cat = cat
+    @date = date
+    @text = text
+    @link = link
+  end
+end
+
+@Jobs = Array.new
+
+agent = Mechanize.new
+@date
+
+#change this to the cities you want
+locs = ['milwaukee', 'madison']
+#change this to the categories you want
+cats = ['sof', 'sad', 'tch', 'web']
+
+locs.each do |loc|
+  cats.each do |cat|
+    page = agent.get("http://#{loc}.craigslist.org/#{cat}/")
+    page.search('//h4[@class="ban"]|//p/a').each do |found|
+      if(found.name=="h4")#date
+        #  @date = found.text[4, found.text.length]
+        # @date = (found.text=="" ? "?" : Time.strftime(found.text[4, found.text.length]+ "%b %d"))
+        if(found.text != "")
+          @date = Time.parse(found.text)
+          if(@date > Time.now)
+            @date = Time.parse(found.text + " 2010")
+            end
+        end
+      else
+        @Jobs << Job.new(loc, 
+                         (found.next.next==nil ? "" : found.next.next.text),
+                         cat,
+                         @date, 
+                         found.text,
+                         "<a  href='" + found.attributes["href"].value + "'>" + found.text[0,40] + "</a>"
+                         )
+
+      end
+    end
+  end
+end
+
+
+newFile = File.open("cl.html", 'w')
+newFile.syswrite(<<-eos
+<html>
+<body>
+<table class='sortable' id='jobs'>
+<thead><tr>
+<th scope=col>Date</th>
+<th scope=col>Title</th>
+<th scope=col>Cat</th>
+<th scope=col>Loc</th>
+<th scope=col>Area</th>
+</thead>
+<tbody>
+eos
+)
+@Jobs.each do |job|
+  newFile.syswrite("<tr><td>" + job.date.strftime("%Y/%m/%d") + "</td><td>#{job.link}</td><td>#{job.cat}</td><td>#{job.loc}</td><td>#{job.area}&nbsp;</td></tr>")
+end
