ef4125ce252cbad37a7a6932d44928aaaa3ed850
Ryland Herrick <ryland@bendyworks.com>
2011-10-23 23:04:50 -0500

    Cleaned up some logic, parens, comments

--- cl.rb
+++ cl.rb
@@ -27,14 +27,10 @@ locs.each do |loc|
   cats.each do |cat|
     page = agent.get("http://#{loc}.craigslist.org/#{cat}/")
     page.search('//h4[@class="ban"]|//p/a').each do |found|
-      if(found.name=="h4")#date
-        #  @date = found.text[4, found.text.length]
-        # @date = (found.text=="" ? "?" : Time.strftime(found.text[4, found.text.length]+ "%b %d"))
-        if(found.text != "")
-          @date = Time.parse(found.text)
-          if(@date > Time.now)
-            @date = Time.parse(found.text + " 2010")
-          end
+      if found.name=="h4" && !found.text.empty?
+        @date = Time.parse(found.text)
+        if @date > Time.now
+          @date = Time.parse(found.text + " 2010")
         end
       else
         @Jobs << Job.new(loc,
@@ -52,19 +48,19 @@ end
 
 
 newFile = File.open("craigs_jobs.html", 'w')
-newFile.syswrite("<html><body>
+newFile.syswrite "<html><body>
                   <table class='sortable' id='jobs'><thead>
                   <tr><th scope=col>Date</th>
                   <th scope=col>Title</th>
                   <th scope=col>Cat</th>
                   <th scope=col>Loc</th>
                   <th scope=col>Area</th>
-                  </tr></thead><tbody>")
+                  </tr></thead><tbody>"
 
 @Jobs.each do |job|
-  newFile.syswrite("<tr><td>#{job.date.strftime("%Y/%m/%d")}</td>
+  newFile.syswrite "<tr><td>#{job.date.strftime("%Y/%m/%d")}</td>
                    <td>#{job.link}</td>
                    <td>#{job.cat}</td>
                    <td>#{job.loc}</td>
-                   <td>#{job.area}&nbsp;</td></tr>")
+                   <td>#{job.area}&nbsp;</td></tr>"
 end
