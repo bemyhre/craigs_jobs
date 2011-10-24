c8485e92c44033b8a556812e38861cab372c58e5
Ryland Herrick <ryland@bendyworks.com>
2011-10-23 23:40:32 -0500

    Interpolation is fun

--- cl.rb
+++ cl.rb
@@ -13,7 +13,7 @@ locations.each do |location|
   categories.each do |category|
     page = agent.get("http://#{location}.craigslist.org/#{category}/")
     page.search('//h4[@class="ban"]|//p/a').each do |found|
-      if found.name=="h4" && !found.text.empty?
+      if found.name=='h4' && !found.text.empty?
         @date = Time.parse(found.text)
         @date = Time.parse("#{found.text} #{Time.now.year-1}") if @date > Time.now
       else
@@ -21,13 +21,13 @@ locations.each do |location|
                   area: (found.next.next.nil? ? "" : found.next.next.text),
                   category: category,
                   date: @date,
-                  link: "<a  href='" + found.attributes["href"].value + "'>" + found.text[0,40] + "</a>" }
+                  link: "<a href='#{found.attributes['href'].value}'>#{found.text[0,40]}</a>" }
       end
     end
   end
 end
 
-newFile = File.open("craigs_jobs.html", 'w')
+newFile = File.open('craigs_jobs.html', 'w')
 newFile.syswrite "<html><body>
                   <table class='sortable' id='jobs'><thead>
                   <tr><th scope=col>Date</th>
